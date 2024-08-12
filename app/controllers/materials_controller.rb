36474d4b424b958243fd41c9bf4859d5a8a660f1
Stephen Steiner <46692873+stephensteiner4@users.noreply.github.com>
2024-08-12 23:14:45 +0000

    include all editable fields on Materials update page

--- app/controllers/materials_controller.rb
+++ app/controllers/materials_controller.rb
@@ -65,25 +65,40 @@ class MaterialsController < ApplicationController
   def update
     the_id = params.fetch("path_id")
     the_material = Material.where({ :id => the_id }).at(0)
-
     the_material.description = params.fetch("query_description")
     the_material.crop_time = params.fetch("query_crop_time")
-    the_material.plan_id = params.fetch("query_plan_id")
-    the_material.material_cost = params.fetch("query_material_cost")
-    the_material.retail_week = params.fetch("query_retail_week")
-    the_material.container_size = params.fetch("query_container_size")
-    the_material.soil_cost = params.fetch("query_soil_cost")
-    the_material.unit_price = params.fetch("query_unit_price")
-    the_material.unit_margin = params.fetch("query_unit_margin")
-    the_material.buffer = params.fetch("query_buffer")
-    the_material.bench_space = params.fetch("query_bench_space")
-    the_material.unit_cost = params.fetch("query_unit_cost")
-    the_material.sqft_cost = params.fetch("query_sqft_cost")
-    the_material.user_id = params.fetch("query_user_id")
-    the_material.sqft_margin = params.fetch("query_sqft_margin")
     the_material.container_type = params.fetch("query_container_type")
+    the_material.container_size = params.fetch("query_container_size")
+    the_material.total_qty = params.fetch("query_total_qty")
+
+    the_material.material_cost = params.fetch("query_material_cost")
     the_material.unit_container_cost = params.fetch("query_unit_container_cost")
     the_material.unit_tag_cost = params.fetch("query_unit_tag_cost")
+    the_material.miscellaneous = params.fetch("query_miscellaneous")
+    
+    the_material.buffer = params.fetch("query_buffer")
+    
+    the_material.unit_price = params.fetch("query_unit_price")
+    
+    the_material.user_id = params.fetch("query_user_id").to_i
+    the_material.plan_id = params.fetch("query_plan_id").to_i
+
+    diam = the_material.container_size
+    
+    the_material.bench_space = ((diam**2/144.0) * the_material.total_qty).round(2)
+    the_material.total_bench_space_weeks = the_material.bench_space * the_material.crop_time
+    prodplan = ProductionPlan.where({:id=>params.fetch("query_plan_id").to_i}).at(0)
+
+    if the_material.container_type == "Pot"
+      the_material.soil_cost = (((3.14 * ((diam/2)**2) * diam) / 46656.0) * prodplan.soil_cost).round(2)
+    end
+    
+    the_material.shrink_opportunity_cost = ((the_material.buffer/100.0) * the_material.unit_price).round(2)
+
+    the_material.unit_cost = the_material.shrink_opportunity_cost + the_material.unit_container_cost + the_material.unit_tag_cost + the_material.material_cost + the_material.soil_cost
+
+    the_material.total_cost = the_material.unit_cost * the_material.total_qty
+    the_material.total_revenue = the_material.unit_price * the_material.total_qty
 
     if the_material.valid?
       the_material.save
